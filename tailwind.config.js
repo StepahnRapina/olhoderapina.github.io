8167278142fbf4f65560790e5da2b0dd77dec91c
StepahnRapina <stecarvalhomilanez@gmail.com>
2025-08-26 02:44:08 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        amarelo: "#FFD700"
+      },
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"]
+      }
+    }
+  },
+  plugins: [],
+};
