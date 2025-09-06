fbd23dd12b7f9d27caa131415639b0c070d5877f
StepahnRapina <stecarvalhomilanez@gmail.com>
2025-09-06 00:13:38 -0300

    up

--- data/destaque.js
+++ data/destaque.js
@@ -1,34 +1,34 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Função para buscar os dados do JSON
-    async function carregarDestaque() {
-        try {
-            const resposta = await fetch('./content/destaque.json');
-            if (!resposta.ok) {
-                throw new Error(`Erro ao carregar o JSON: ${resposta.statusText}`);
-            }
-            const dadosDestaque = await resposta.json();
-            
-            // Chama a função para renderizar o HTML
-            renderizarDestaque(dadosDestaque);
-        } catch (erro) {
-            console.error('Falha ao carregar o destaque:', erro);
-        }
+document.addEventListener("DOMContentLoaded", () => {
+  // Função para buscar os dados do JSON
+  async function carregarDestaque() {
+    try {
+      const resposta = await fetch("./content/destaque.json");
+      if (!resposta.ok) {
+        throw new Error(`Erro ao carregar o JSON: ${resposta.statusText}`);
+      }
+      const dadosDestaque = await resposta.json();
+
+      // Chama a função para renderizar o HTML
+      renderizarDestaque(dadosDestaque);
+    } catch (erro) {
+      console.error("Falha ao carregar o destaque:", erro);
+    }
+  }
+
+  // Função para preencher o HTML com os dados do JSON
+  function renderizarDestaque(dados) {
+    const divDestaque = document.querySelector("[data-destaque]");
+    if (!divDestaque) {
+      console.error("Div de destaque não encontrada.");
+      return;
     }
 
-    // Função para preencher o HTML com os dados do JSON
-    function renderizarDestaque(dados) {
-        const divDestaque = document.querySelector('[data-destaque]');
-        if (!divDestaque) {
-            console.error('Div de destaque não encontrada.');
-            return;
-        }
-
-        const htmlDinamico = `
-            <a href="${dados.link}" class="hover:text-yellow-500 text-2xl font-bold mb-2">
+    const htmlDinamico = `
+            <a href="${dados.link}#leitura" class="hover:text-yellow-500 text-2xl font-bold mb-2">
                 ${dados.titulo}
             </a>
             <br>
-            <a href="${dados.link}">
+            <a href="${dados.link}#leitura">
                 <img
                     src="${dados.imagemSrc}"
                     alt="${dados.imagemAlt}"
@@ -42,10 +42,10 @@ document.addEventListener('DOMContentLoaded', () => {
                 </a>
             </p>
         `;
-        
-        divDestaque.innerHTML = htmlDinamico;
-    }
 
-    // Executa a função
-    carregarDestaque();
+    divDestaque.innerHTML = htmlDinamico;
+  }
+
+  // Executa a função
+  carregarDestaque();
 });
